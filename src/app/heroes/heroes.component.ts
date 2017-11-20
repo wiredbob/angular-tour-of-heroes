aa91f5b73b7326e01628f0ea839ddf9298265014
Robert Conn <robert@wiredbob.com>
2017-11-20 21:53:31 +0000

    Introduced hero class

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +7,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./heroes.component.css'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+  	id: 1,
+  	name: 'Windstorm'
+  }
 
   constructor() { }
 
