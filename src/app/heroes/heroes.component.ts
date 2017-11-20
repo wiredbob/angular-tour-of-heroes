d784c2d4fd8d7e153921b04457f9e343e6d1fb64
Robert Conn <robert@wiredbob.com>
2017-11-20 21:17:04 +0000

    Angular app and hero components

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeroesComponent implements OnInit {
+
+  hero = 'Windstorm';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
